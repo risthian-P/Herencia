b9f3998d90ec1dbf9d3d1adf9de00b1b852581ef
Cristhian Panora <cristhian.panora@gmail.com>
2023-06-13 08:11:20 -0500

    herencia en java

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,6 +1,11 @@
-public class Estudiante {
+public class Estudiante extends Persona{
     private int codigo;
     private String correo;
     private String semestre;
     private String carrera;
+
+    public Estudiante(int ci, String nombre, String apellido, String direccion, String telefono) {
+        super(ci, nombre, apellido, direccion, telefono);
+    }
+
 }
